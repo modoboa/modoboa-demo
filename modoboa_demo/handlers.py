893481eb1f96300faf303616505d8cdeac07b952
Antoine Nguyen <tonio@ngyn.org>
2025-07-03 11:27:10 +0200

    Compat with modoboa 2.4

--- modoboa_demo/handlers.py
+++ modoboa_demo/handlers.py
@@ -7,30 +7,29 @@ from django.utils.translation import gettext as _
 
 
 from modoboa.core import signals as core_signals
-from modoboa.lib.web_utils import static_url
 
 
-@receiver(core_signals.extra_user_menu_entries)
-def menu(sender, location, user, **kwargs):
-    if location != "top_menu_middle":
-        return []
-    if not hasattr(user, "mailbox"):
-        return []
-    return [
-        {"name": "demo",
-         "label": _("Test messages"),
-         "img": static_url("pics/demo.png"),
-         "class": "topdropdown",
-         "menu": [
-             {"name": "sendvirus",
-              "label":  _("Send virus"),
-              "url": reverse("modoboa_demo:virus_send")},
-             {"name": "sendspam",
-              "label":  _("Send spam"),
-              "url": reverse("modoboa_demo:spam_send")}
-         ]
-         }
-    ]
+# @receiver(core_signals.extra_user_menu_entries)
+# def menu(sender, location, user, **kwargs):
+#     if location != "top_menu_middle":
+#         return []
+#     if not hasattr(user, "mailbox"):
+#         return []
+#     return [
+#         {"name": "demo",
+#          "label": _("Test messages"),
+#          "img": static_url("pics/demo.png"),
+#          "class": "topdropdown",
+#          "menu": [
+#              {"name": "sendvirus",
+#               "label":  _("Send virus"),
+#               "url": reverse("modoboa_demo:virus_send")},
+#              {"name": "sendspam",
+#               "label":  _("Send spam"),
+#               "url": reverse("modoboa_demo:spam_send")}
+#          ]
+#          }
+#     ]
 
 
 @receiver(core_signals.get_announcements)
@@ -46,14 +45,14 @@ def password_change(sender, user, **kwargs):
     return [user.id == 1]
 
 
-@receiver(core_signals.extra_static_content)
-def get_static_content(sender, caller, st_type, user, **kwargs):
-    if caller != 'top' or st_type != 'js' or not hasattr(user, "mailbox"):
-        return ""
+# @receiver(core_signals.extra_static_content)
+# def get_static_content(sender, caller, st_type, user, **kwargs):
+#     if caller != 'top' or st_type != 'js' or not hasattr(user, "mailbox"):
+#         return ""
 
-    return """<script type="text/javascript">
-$(document).ready(function() {
-    $(document).on('click', 'a[name=sendspam]', simple_ajax_request);
-    $(document).on('click', 'a[name=sendvirus]', simple_ajax_request);
-});
-</script>"""
+#     return """<script type="text/javascript">
+# $(document).ready(function() {
+#     $(document).on('click', 'a[name=sendspam]', simple_ajax_request);
+#     $(document).on('click', 'a[name=sendvirus]', simple_ajax_request);
+# });
+# </script>"""
